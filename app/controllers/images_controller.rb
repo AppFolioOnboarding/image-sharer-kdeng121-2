58b1728f1534a1d887ad1a578b12e5ed1f452d51
Kevin Deng <kevin.deng@appfolio.com>
2019-05-22 11:36:11 -0700

    kd - Update Images Controller and package-lock.json

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,5 +3,22 @@ class ImagesController < ApplicationController
     @image = Image.new
   end
 
-  def create; end
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end

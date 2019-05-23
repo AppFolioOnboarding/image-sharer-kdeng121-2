0983f42e3c4f42afebac09db73327a153ea0a4b8
Kevin Deng <kevin.deng@appfolio.com>
2019-05-23 13:06:20 -0700

    kd - Add Image model test

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -4,4 +4,16 @@ class ImageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'URL is not blank' do
+    image = Image.new
+    assert_not image.save, 'Saved image with blank URL'
+  end
+
+  test 'URL is valid' do
+    image = Image.new
+    image.url = 'test.com'
+    assert_not image.save
+    image.url = 'http://test.com'
+    assert image.save
+  end
 end

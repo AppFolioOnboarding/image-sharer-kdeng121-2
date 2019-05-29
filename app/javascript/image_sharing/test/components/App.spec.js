50b800252be3d507dce3e93ece321c5cf6f51bb4
Kevin Deng <kevin.deng@appfolio.com>
2019-05-29 09:14:44 -0700

    kd - Update homepage to include feedback form and footer

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -1,13 +1,22 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
 
 describe('<App />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<App />);
-    assert(wrapper.contains('Tell us what you think'));
+    const wrapper = shallow(<App />);
+
+    const header = wrapper.find('Header');
+    assert.strictEqual(header.length, 1);
+    assert.strictEqual(header.prop('title'), 'Tell us what you think');
+
+    const feedbackForm = wrapper.find('FeedbackForm');
+    assert.strictEqual(feedbackForm.length, 1);
+
+    const footer = wrapper.find('Footer');
+    assert.strictEqual(footer.length, 1);
   });
 });
